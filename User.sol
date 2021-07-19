917405f27e6b39a55dd4314c767545168c97d4d1
Gaurav7888 <60139335+Gaurav7888@users.noreply.github.com>
2021-07-19 18:52:29 +0530

    Add files via upload

---
+++ User.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.17;
+
+contract User {
+    
+    string private user_name;
+    string private bio_data;
+    ufixed private balance;
+    bool user_type;                     //0 for customer and 1 for driver
+    
+}
